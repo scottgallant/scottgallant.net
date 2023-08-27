d01848c730ab685db354b39808dac80cc9d942ac
Scott Gallant <scottgallant@gmail.com>
2023-08-26 23:42:37 -0300

    updated blog

--- footer.js
+++ footer.js
@@ -1,27 +1,20 @@
 document.write(`
 
 <footer>
-    <!-- Begin Mailchimp Signup Form -->
-    <form
-        action="https://gmail.us9.list-manage.com/subscribe/post?u=24ef9f2e90309a87c9431d8df&amp;id=18a755bc0a&amp;f_id=00181be1f0"
-        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
-        <input type="email" value="" placeholder="Type your email" name="EMAIL" class="required email" id="mce-EMAIL"
-            required>
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-    </form>
-    <p><small>Scott Gallant</small></p>
+   <!-- Buttondown Form -->
+   <form
+   action="https://buttondown.email/api/emails/embed-subscribe/gallant"
+   method="post"
+   target="popupwindow"
+   onsubmit="window.open('https://buttondown.email/gallant', 'popupwindow')"
+   class="embeddable-buttondown-form"
+   >
+      <input type="email" name="email" id="bd-email" aria-label="Email Address" placeholder="Email Address" />
+      <input type="submit" value="Subscribe" />
+   </form>
 
-    <!--End mc_embed_signup-->
+   <p><small>Scott Gallant</small></p>
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X9DK1ZSGLX"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-X9DK1ZSGLX');
-    </script>
 </footer>
 
 `);
